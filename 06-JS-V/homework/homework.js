2acbd7ab0282235e707a3644b77a8e066f6e5483
xzetita <67513081+xzetita@users.noreply.github.com>
2020-07-02 16:01:00 -0300

    JSV completed

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,15 +8,29 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  class Usuario {
+    constructor (opciones) {
+      this.usuario = opciones.usuario,
+      this.nombre = opciones.nombre,
+      this.email = opciones.email,
+      this.password = opciones.password
+    }
+    saludar () {
+      return ("Hola, mi nombre es " + this.nombre);
 
+  }
+  }
+  return Usuario;
 }
-
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function () {
+    return "Hello World!";
 
-}
+        }
+  }
 
 function agregarStringInvertida() {
   // Agrega un método al prototype de String que devuelva la misma cadena de caracteres, pero invertida.
@@ -24,7 +38,13 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-
+ String.prototype.reverse = function (){
+   var invertida = '';
+   for (var i = this.length - 1; i >= 0; i--){
+    invertida = invertida + this.charAt(i);
+  }
+    return invertida;
+ }
 }
 
 // No modificar nada debajo de esta línea
